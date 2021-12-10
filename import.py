cf00549187fb4952ff009e988188f9d2256fdc91
Gabriel Ferrara <gabrielbarbosa_ferrara@hotmail.com>
2021-12-09 22:45:15 -0300

    Adicionado a melhoria para executar via argumentos delinha de comando, limpeza de comentarios antigos no RN412, adicionado --help

--- import.py
+++ import.py
@@ -1,41 +1,42 @@
+import sys
 import csv
 from RN412 import calculo_rn412
-
 import pandas as pd
+
+
 
 #Arquivo deverá conter da coluna A até a E os seguintes cabeçalhos: NOME, MENSALIDADE, VIGENCIA, DT_CANCELAMENTO, ULTIMO_MES_FATURAMENTO
 #se atentar ao colocar a coluna valor separando decimais com "." ao inves de "," e deixar como texto as colunas "D" e "E" (DT_CANCELAMENTO e ULTIMO_MES_FATURAMENTO)
 
-tipo_analise = 'None'
-erro = 0
-
-
-while (tipo_analise != 'L' and tipo_analise != 'I'):    
-    if(erro == 0):
-        tipo_analise = input('Deseja calcular individual (I) ou em lote (L): ').upper()
-        erro += 1
-    else:
-        print('Entrada incorreta')
-        tipo_analise = input('Deseja calcular individual (I) ou em lote (L): ').upper()
-
 
 def analise_individual():
-
-    mensalidade = float(input('Informe o valor da mensalidade R$####.##: '))
-    vigencia = int(input('Informe a vigencia: '))
-    dt_cancelamento = input('Informe a data de cancalemnto ##/##/####: ')
-    ultimo_mes_faturamento = input('Informe a ultima competencia faturada ##/####: ')
+    if(len(sys.argv) == 5):
+        mensalidade = float(sys.argv[1])
+        vigencia = int(sys.argv[2])
+        dt_cancelamento = sys.argv[3]
+        ultimo_mes_faturamento = sys.argv[4]
+    else:
+        mensalidade = float(input('Informe o valor da mensalidade R$####.##: '))
+        vigencia = int(input('Informe a vigencia: '))
+        dt_cancelamento = input('Informe a data de cancalemnto ##/##/####: ')
+        ultimo_mes_faturamento = input('Informe a ultima competencia faturada ##/####: ')
 
     return print(calculo_rn412(mensalidade, vigencia, dt_cancelamento, ultimo_mes_faturamento))
 
 def analise_lote():
-    
-    arquivo_leitura = pd.read_csv(r'/home/gabsferrara/projects/RN412/RN412.CSV', sep=';', encoding='iso8859-1')
+    if(len(sys.argv) == 3):
+        diretorio = sys.argv[1]
+        diretorio_retorno = sys.argv[2]
+    else:
+        diretorio = input('Informar diretorio + nome_arquivo.csv :')
+        diretorio_retorno = input('Informar diretorio retorno + nome_arquivo_retorno.csv :')
+        
+    arquivo_leitura = pd.read_csv(diretorio, sep=';', encoding='iso8859-1')
     #Ao ler o arquivo a biblioteca do pandas entende object = string
 
     tuples = [tuple(x) for x in arquivo_leitura.values]
 
-    arquivo_retorno = open('/home/gabsferrara/projects/RN412/arquivo_retorno.csv', 'w', newline='', encoding='iso8859-1')
+    arquivo_retorno = open(diretorio_retorno, 'w', newline='', encoding='iso8859-1')
 
     objeto_gravacao = csv.writer(arquivo_retorno)
 
@@ -46,7 +47,38 @@ def analise_lote():
     arquivo_retorno.close()
     print('Processo concluido')
 
-if(tipo_analise == 'I'):
-    analise_individual()
-else:
+
+def rodando_usuario():
+
+    tipo_analise = 'None'
+    erro = 0
+
+    while (tipo_analise != 'L' and tipo_analise != 'I'):    
+        if(erro == 0):
+            tipo_analise = input('Deseja calcular individual (I) ou em lote (L): ').upper()
+            erro += 1
+        else:
+            print('Entrada incorreta')
+            tipo_analise = input('Deseja calcular individual (I) ou em lote (L): ').upper()
+    if(tipo_analise == 'I'):
+        analise_individual()
+    else:
+        analise_lote()
+
+
+if(len(sys.argv)<2):
+    rodando_usuario()
+
+elif(sys.argv[1] == '--help'):
+    print('Para utilizar o programa através de parametros inputados diretamente no console voce tem duas opções: ')
+    print('1) Indidual - com isso os parametros são mensalidade(####.##), vigencia(##), dt_cancelamento(##/##/####) e ultimo_mes_faturamento(##/####)')
+    print('2) Lote - com isso os parametros são diretorio/nome_arquivo.csv e diretorio_retorno/nome_arquivo_retorno.csv ')
+
+elif(len(sys.argv) == 3):
     analise_lote()
+
+elif(len(sys.argv) == 5):
+    analise_individual()
+
+else:
+    print('Parametros invalidos, consulte --help')
